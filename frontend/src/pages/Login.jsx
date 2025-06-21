1e09c095237ac52bd56590b7f663cec011883732
Devendra-Pudi <pudidevendraprasad@gmail.com>
2025-06-21 10:50:45 +0530

    Update Login.jsx

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -12,7 +12,7 @@ export default function Login() {
     e.preventDefault();
     setError("");
     try {
-      const res = await axios.post("http://localhost:5000/api/auth/login", { email, password });
+      const res = await axios.post("https://task-management-app-omega-tawny.vercel.app/api/auth/login", { email, password });
       localStorage.setItem("token", res.data.token);
       navigate("/");
     } catch (err) {
