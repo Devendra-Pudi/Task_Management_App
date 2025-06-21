f11a58e387c5712d48c8fc1a3c2b22587f21103c
Devendra-Pudi <pudidevendraprasad@gmail.com>
2025-06-21 10:51:12 +0530

    Update Register.jsx

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -13,7 +13,7 @@ export default function Register() {
     e.preventDefault();
     setError("");
     try {
-      const res = await axios.post("http://localhost:5000/api/auth/signup", form);
+      const res = await axios.post("https://task-management-app-omega-tawny.vercel.app/api/auth/signup", form);
       localStorage.setItem("token", res.data.token);
       navigate("/");
     } catch (err) {
